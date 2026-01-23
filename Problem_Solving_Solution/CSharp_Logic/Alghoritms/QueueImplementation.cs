c7db37e8a530650d44d8c7ca5f89b32cd2512e48
Creed <laurvitalik89@gmail.com>
2026-01-23 20:44:06 +0200

    feat[cs] Implement Queue in C#

---
+++ Problem_Solving_Solution/CSharp_Logic/Alghoritms/QueueImplementation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Logic.Alghoritms
+{
+    public class QueueImplementation
+    {
+        public static void Run()
+        {
+            // We use the buitl-in Queue class from System.Collections.Generic
+            Queue<string> superMarketQueue = new Queue<string>();
+
+            // Enqueue - Adding elements to the end
+            superMarketQueue.Enqueue("Jhon");
+            superMarketQueue.Enqueue("Alice");
+            superMarketQueue.Enqueue("Bob");
+
+            Console.WriteLine("Initial queue count: " + superMarketQueue.Count );
+
+            // Peek - Look at the person in front without removing them
+            Console.WriteLine("Person in front: " + superMarketQueue.Peek());
+
+            // Dequeue - Remove the person from the front
+            string removedperson = superMarketQueue.Dequeue();
+            Console.WriteLine($"Served: {removedperson}. Remaining: {superMarketQueue.Count}");
+
+            //Iterating through the queue
+            Console.WriteLine("Remaining people in queue:");
+            foreach(var person in superMarketQueue)
+            {
+                Console.WriteLine("- " + person);
+            }
+        }
+    }
+}
